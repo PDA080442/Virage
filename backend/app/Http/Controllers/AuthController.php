600db55897267dc438ab22f954fc9b608ae06b11
Данил Поправкин <danilpopravkin@244080.local>
2026-07-03 16:43:17 +0700

    feat: add AuthController@register

---
+++ backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules\Password;
+
+class AuthController extends Controller
+{
+    public function register(Request $request): JsonResponse
+    {
+        $validated = $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'confirmed', Password::defaults()],
+        ]);
+
+        $user = User::create($validated);
+
+        Auth::login($user);
+
+        return response()->json($user);
+    }
+}
