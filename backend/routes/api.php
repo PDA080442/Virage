0c80c0b178648bc04768eddf505c7caf6a681fc5
Данил Поправкин <danilpopravkin@244080.local>
2026-07-03 17:36:59 +0700

    feat: Routing

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -7,12 +7,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
-Route::get('/user', [AuthController::class, 'user'])->middleware('auth:sanctum');
 
 Route::get('/products', [ProductController::class, 'index']);
 
 Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', [AuthController::class, 'user']);
+
     Route::get('/me/favorites', [FavoriteController::class, 'index']);
     Route::post('/favorites/{product}', [FavoriteController::class, 'store']);
     Route::delete('/favorites/{product}', [FavoriteController::class, 'destroy']);
