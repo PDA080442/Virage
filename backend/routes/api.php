9deba4fbb36a7c04bf51b20811b17023f72f97ec
Данил Поправкин <danilpopravkin@244080.local>
2026-07-03 16:59:34 +0700

    feat: ProductController@index

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,9 +1,12 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 Route::get('/user', [AuthController::class, 'user'])->middleware('auth:sanctum');
+
+Route::get('/products', [ProductController::class, 'index']);
