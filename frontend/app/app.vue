4fe3c31c21e88e86ec648e9c92fabc173eea3b1d
Данил Поправкин <danilpopravkin@244080.local>
2026-07-03 18:43:21 +0700

    infr: init nuxt, connect tailwind

---
+++ frontend/app/app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="p-8 text-xl font-bold text-blue-600">
+    Tailwind works
+  </div>
+</template>
---
+++ frontend/nuxt.config.ts
@@ -0,0 +1,21 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/a11y',
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxt/hints',
+    '@nuxt/image',
+    '@nuxt/scripts',
+    '@nuxt/test-utils',
+    '@nuxt/ui',
+    '@nuxtjs/tailwindcss',
+  ],
+
+  tailwindcss: {
+    cssPath: '~/assets/css/tailwind.css',
+  },
+})
